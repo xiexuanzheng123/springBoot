46276045b804f165c0abd5de5c3f333725c449f3
xiexuanzheng123 <349328984@qq.com>
2018-09-09 15:24:00 +0800

    add notation

--- src/main/java/com/wisely/highlight_spring4/ch1/di/DiConfig.java
+++ src/main/java/com/wisely/highlight_spring4/ch1/di/DiConfig.java
@@ -3,7 +3,7 @@ package com.wisely.highlight_spring4.ch1.di;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration//声明当前类是一个配置类
+@Configuration//声明当前类是一个配置类,相当于一个Spring配置的xml文件
 @ComponentScan("com.wisely.highlight_spring4.ch1.di")
 //自动扫描包名下所有使用@Service、@Component、@Repository、@Controller的类并注册为Bean
 
